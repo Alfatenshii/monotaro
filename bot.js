82da8ed9c9772ce33a2bc0db54d6659983e3343d
Alfatenshii <36442686+Alfatenshii@users.noreply.github.com>
2018-02-13 13:49:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const music = require('discord.js-music-v11');
 const Bot = new Discord.Client();
-const token = "<auth_token>" // Recommended to load from json file.
 
 Bot.on('ready', () => {
     console.log(`[Start] ${new Date()}`);
